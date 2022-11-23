b4391446dfb25e5b41cd8898f474b16d9343201f
angelinesimiyu <angie.lukelas@gmail.com>
2022-11-23 15:48:40 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 
 // Write your code here
+
+const num1 = 2;
+const num2 = 31;
+
+const multiply = num1 * num2;
+
+const random = 5
+Number.isInteger(random);
+
+
+const num3 = 28;
+const num4 = 7;
+const mod = num3 / num4;
+
+const max = 20
+Math.max(1, 3, max);
+
+
+
+
